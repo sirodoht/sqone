6d1c926044c3041f8154117178bc411d0720820e
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-03-15 20:13:14 +0200

    add unique image route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.set('Content-Length', 13945);
-  res.set('Content-Type', 'image/png');
-  next();
+router.get('/', (req, res) => {
+  res.send('Well, hello there!');
+});
+
+/* GET unique image. */
+router.get('/:id', (req, res) => {
+  const options = {
+    root: __dirname + '/../public/images/',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true,
+    },
+  };
+
+  res.sendFile('divider.png', options, err => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', req.url);
+    }
+  });
 });
 
 module.exports = router;
