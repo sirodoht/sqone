7dc687c8c36fdf179373e5b9224b12b36a7b5fe2
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-03-15 20:54:22 +0200

    implement redis save method

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Redis = require('ioredis');
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -8,21 +9,42 @@ router.get('/', (req, res) => {
 
 /* GET unique image. */
 router.get('/:id', (req, res) => {
+  const redis = new Redis({
+    port: 6379,
+    host: '127.0.0.1',
+    db: 2,
+  });
+
   const options = {
     root: __dirname + '/../public/images/',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true,
-    },
   };
+  const keyName = req.params.id;
 
-  res.sendFile('divider.png', options, err => {
-    if (err) {
-      next(err);
-    } else {
-      console.log('Sent:', req.url);
-    }
-  });
+  redis
+    .get(keyName)
+    .then(result => {
+      console.log('result:', result);
+      let newValue = 1;
+      if (result) {
+        newValue = parseInt(result) + 1;
+      }
+      return newValue;
+    })
+    .then(newValue => {
+      return redis.set(keyName, newValue);
+    })
+    .then(() => {
+      return res.sendFile('divider.png', options, err => {
+        if (err) {
+          next(err);
+        } else {
+          console.log('Key name saved:', keyName);
+        }
+      });
+    })
+    .catch(err => {
+      throw err;
+    });
 });
 
 module.exports = router;
