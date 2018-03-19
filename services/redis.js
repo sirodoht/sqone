9e5e39e63eadbcfdfce85264b44702a680684b93
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-03-19 16:18:16 +0200

    add redis service

---
+++ services/redis.js
@@ -0,0 +1,19 @@
+const Redis = require('ioredis');
+
+const redisService = {};
+
+let connection = null;
+
+redisService.connect = function(namespaceKey = 'sqone:') {
+  connection = new Redis(process.env.REDIS_URL, {keyPrefix: namespaceKey});
+};
+
+redisService.disconnect = function() {
+  connection.disconnect();
+};
+
+redisService.connection = function() {
+  return connection;
+};
+
+module.exports = redisService;
